fb13597be71ed8d6a6a07fa7ec1c2cdaf4fe1627
Alex Liberzon <alex.liberzon@gmail.com>
2011-04-27 07:20:39 +0300

    - removed obsolete gauss_kernel

--- openpiv/pyprocess.py
+++ openpiv/pyprocess.py
@@ -35,28 +35,6 @@ import pdb
 
 
 
-# def gauss_kern(size, sizey=None):
-#     """ Returns a normalized 2D gauss kernel array for convolutions 
-#         from http://www.scipy.org/Cookbook/SignalSmooth
-#     """
-#     size = int(size)
-#     if not sizey:
-#         sizey = size
-#     else:
-#         sizey = int(sizey)
-#     x, y = np.mgrid[-size:size+1, -sizey:sizey+1]
-#     g = np.exp(-(x**2/float(size) + y**2/float(sizey)))
-#     return g / g.sum()
-# 
-# def blur_image(im, n, ny=None) :
-#     """ blurs the image by convolving with a gaussian kernel of typical
-#         size n. The optional keyword argument ny allows for a different
-#         size in the y direction.
-#         from http://www.scipy.org/Cookbook/SignalSmooth
-#     """
-#     g = gauss_kern(n, sizey=ny)
-#     improc = signal.convolve(im, g, mode='same')
-#     return improc 
 
 def get_coordinates( image_size, window_size, overlap ):
     """Compute the x, y coordinates of the centers of the interrogation windows.
