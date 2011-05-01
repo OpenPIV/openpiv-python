f9df7c326ef13c44bb4402d9da1f0930b93e54f8
Alex Liberzon <alex.liberzon@gmail.com>
2011-05-01 19:47:09 +0300

    had to update example.1 to work with the new merged changes in tools, validation and filters

--- openpiv/docs/examples/example1/example1.py
+++ openpiv/docs/examples/example1/example1.py
@@ -17,14 +17,14 @@ u, v, sig2noise = openpiv.process.extended_search_area_piv( frame_a, frame_b, wi
 # get window centers coordinates
 x, y = openpiv.pyprocess.get_coordinates( image_size=frame_a.shape, window_size=32, overlap=16 )
 
-u, v = openpiv.validation.sig2noise_val( u, v, sig2noise, threshold = 1.3 )
+u, v, mask = openpiv.validation.sig2noise_val( u, v, sig2noise, threshold = 1.3 )
 
-u, v = openpiv.filters.replace_outliers( u, v, method='localmean', n_iter=10, kernel_size=2)
+u, v = openpiv.filters.replace_outliers( u, v, method='localmean', max_iter=10, kernel_size=2)
 
 x, y = openpiv.pyprocess.get_coordinates( image_size=frame_a.shape, window_size=24, overlap=12 )
 
 x, y, u, v = openpiv.scaling.uniform(x, y, u, v, scaling_factor = 96.52 )
 
-openpiv.tools.save(x, y, u, v, 'exp1_001.txt' )
+openpiv.tools.save(x, y, u, v, mask, 'exp1_001.txt' )
     
 openpiv.tools.display_vector_field( 'exp1_001.txt' )    
