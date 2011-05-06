1a42217d72972b136c81884cf4f701622a4491e0
Davide Lasagna <lasagnadavide@gmail.com>
2011-05-06 19:08:54 +0200

    Created correlation function class in process.pyx

--- openpiv/src/process.pyx
+++ openpiv/src/process.pyx
@@ -1,7 +1,9 @@
 """A cython module for fast advanced algorithms for PIV image analysis."""
 
 import numpy as np
+import numpy.ma as ma
 import openpiv.pyprocess
+from math import log
 
 cimport numpy as np
 cimport cython
@@ -173,4 +175,177 @@ def extended_search_area_piv( np.ndarray[DTYPEi_t, ndim=2] frame_a,
         return u, v, sig2noise
     else:
         return u, v
+    
+    
+class CorrelationFunction( ):
+    def __init__ ( self, corr ):
+        self.data = corr
+        
+        self.peak1, self.corr_max1 = self._find_peak( self.data )
+        
+    def _find_peak ( self, array ):
+        """Find row and column indices of the highest peak in an array."""    
+        ind = array.argmax()
+        s = array.shape[1] 
+        
+        i = ind // s 
+        j = ind %  s
+        
+        return  (i, j),  array.max()
+        
+    def _find_second_peak ( self, width ):
+        """
+        Find the value of the second largest peak.
+        
+        The second largest peak is the height of the peak in 
+        the region outside a widthxwidth submatrix around the first 
+        correlation peak.
+        
+        Parameters
+        ----------
+        width : int
+            the half size of the region around the first correlation 
+            peak to ignore for finding the second peak.
+              
+        Returns
+        -------
+        i, j : two elements tuple
+            the row, column index of the second correlation peak.
+            
+        corr_max2 : int
+            the value of the second correlation peak.
+        
+        """ 
+        # create a masked view of the self.data array
+        tmp = self.data.view(ma.MaskedArray)
+        
+        # set width x width square submatrix around the first correlation peak as masked.
+        # Before check if we are not too close to the boundaries, otherwise we have negative indices
+        iini = max(0, self.peak1[0]-width)
+        ifin = min(self.peak1[0]+width+1, self.data.shape[0])
+        jini = max(0, self.peak1[1]-width)
+        jfin = min(self.peak1[1]+width+1, self.data.shape[1])
+        tmp[ iini:ifin, jini:jfin ] = ma.masked
+        peak, corr_max2 = self._find_peak( tmp )
+        
+        return peak, corr_max2  
+            
+    def subpixel_peak_position( self, method='gaussian' ):
+        """
+        Find subpixel approximation of the correlation peak.
+        
+        This function returns a subpixels approximation of the correlation
+        peak by using one of the several methods available. If requested, 
+        the function also returns the signal to noise ratio level evaluated 
+        from the correlation map.
+        
+        Parameters
+        ----------            
+        method : string
+             one of the following methods to estimate subpixel location of the peak: 
+             'centroid' [replaces default if correlation map is negative], 
+             'gaussian' [default if correlation map is positive], 
+             'parabolic'.
+             
+        Returns
+        -------
+        subp_peak_position : two elements tuple
+            the fractional row and column indices for the sub-pixel
+            approximation of the correlation peak.
+        """
+    
+        # the peak and its neighbours: left, right, down, up
+        try:
+            c  = self.data[self.peak1[0]  , self.peak1[1]  ]
+            cl = self.data[self.peak1[0]-1, self.peak1[1]  ]
+            cr = self.data[self.peak1[0]+1, self.peak1[1]  ]
+            cd = self.data[self.peak1[0]  , self.peak1[1]-1] 
+            cu = self.data[self.peak1[0]  , self.peak1[1]+1]
+        except IndexError:
+            # if the peak is near the border do not 
+            # do subpixel approximation
+            return self.peak1
+            
+        # if correlation is negative near the peak, fall back 
+        # to a centroid approximation
+        if np.any ( np.array([c,cl,cr,cd,cu]) < 0 ) and method == 'gaussian':
+            method = 'centroid'
+        
+        # choose method
+        if method == 'centroid':
+            subp_peak_position = (((self.peak1[0]-1)*cl+self.peak1[0]*c+(self.peak1[0]+1)*cr)/(cl+c+cr),
+                                ((self.peak1[1]-1)*cd+self.peak1[1]*c+(self.peak1[1]+1)*cu)/(cd+c+cu))
+    
+        elif method == 'gaussian':
+            subp_peak_position = (self.peak1[0] + ( (log(cl)-log(cr) )/( 2*log(cl) - 4*log(c) + 2*log(cr) )),
+                                self.peak1[1] + ( (log(cd)-log(cu) )/( 2*log(cd) - 4*log(c) + 2*log(cu) ))) 
+    
+        elif method == 'parabolic':
+            subp_peak_position = (self.peak1[0] +  (cl-cr)/(2*cl-4*c+2*cr),
+                                    self.peak1[1] +  (cd-cu)/(2*cd-4*c+2*cu)) 
+        else:
+            raise ValueError( "method not understood. Can be 'gaussian', 'centroid', 'parabolic'." )
+                
+        return subp_peak_position
+        
+    def sig2noise_ratio( self, method='peak2peak', width=2 ):
+        """
+        Computes the signal to noise ratio from the correlation map.
+        
+        The signal to noise ratio is computed from the correlation map with
+        one of two available method. It is a measure of the quality of the 
+        matching between to interogation windows.
+        
+        Parameters
+        ----------
+        corr : 2d np.ndarray
+        the correlation map.
+        
+        sig2noise_method: string
+            the method for evaluating the signal to noise ratio value from 
+            the correlation map. Can be `peak2peak`, `peak2mean` or None
+            if no evaluation should be made.
+            
+        width : int, optional
+            the half size of the region around the first
+            correlation peak to ignore for finding the second
+            peak. [default: 2]. Only used if ``sig2noise_method==peak2peak``.
+            
+        Returns
+        -------
+        sig2noise : float 
+            the signal to noise ratio from the correlation map.
+            
+        """
+
+        # if the image is lacking particles, totally black it will correlate to very low value, but not zero
+        # return zero, since we have no signal.
+        if self.corr_max1 <  1e-3:
+            return 0.0
+            
+        # if the first peak is on the borders, the correlation map is wrong
+        # return zero, since we have no signal.
+        if ( 0 in self.peak1 or self.data.shape[0] in self.peak1 or self.data.shape[1] in self.peak1):
+            return 0.0
+        
+        # now compute signal to noise ratio
+        if method == 'peak2peak':
+            # find second peak height
+            peak2, corr_max2 = self._find_second_peak( width=width )
+            
+        elif method == 'peak2mean':
+            # find mean of the correlation map
+            corr_max2 = self.data.mean()
+            
+        else:
+            raise ValueError('wrong sig2noise_method')
+    
+        # avoid dividing by zero
+        try:
+            sig2noise = self.corr_max1/corr_max2
+        except ValueError:
+            sig2noise = np.inf    
+            
+        return sig2noise
+    
     
