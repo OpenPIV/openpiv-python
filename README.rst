04b60757cba8849bad716105a12b19e2a5b703ad
Alex Liberzon <alex.liberzon@gmail.com>
2012-12-30 01:02:51 +0200

    added README.rst and preprocess.py
    testing the new documentation build method with a separate directory
    added preprocess.py with the dynamic masking function

---
+++ README.rst
@@ -0,0 +1,39 @@
+=======
+Warning
+=======
+The OpenPIV python version is currently in alpha state. This means that
+it is buggy, untested and the API may change. However testing and contributing
+is very welcome, especially if you can contribute with new algorithms and features.
+
+Development is currently done on a Linux/Mac OSX environment, but as soon as possible 
+Windows will be tested. If you have access to one of these platforms
+please test the code. 
+
+
+=======
+OpenPIV
+=======
+OpenPIV consists in a python module for scripting and executing the analysis of 
+a set of PIV image pairs. In addition, a Qt graphical user interface is in 
+development, to ease the use for those users who don't have python skills.
+
+
+=======
+Install
+=======
+Installation instructions for various platforms can be found at http://alexlib.github.com/openpiv-python
+
+Basically we use distutils:
+
+>>> python setup.py build
+
+should work 
+
+
+=============
+Documentation
+=============
+
+The OpenPiv documentation is available on the project web page at http://alexlib.github.com/openpiv-python
+
+
---
+++ openpiv/preprocess.py
@@ -0,0 +1,51 @@
+"""This module contains image processing routines that improve
+images prior to PIV processing."""
+
+__licence_ = """
+Copyright (C) 2011  www.openpiv.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from skimage import io, img_as_float, img_as_int, exposure, data
+from skimage.util.dtype import dtype_range
+from scipy.ndimage import median_filter, gaussian_filter, binary_fill_holes
+
+
+
+def dynamic_masking(image):
+    """ Dynamically masks out the objects in the PIV images
+    
+    Parameters
+    ----------
+    image: image
+        a two dimensional array of uint16, uint8 or similar type
+            
+    Returns
+    -------
+    image : 2d np.ndarray of floats
+        
+    """
+    image = exposure.rescale_intensity(image, in_range=(0, 1))
+    blurback = gaussian_filter(median_filter(image,size=3),sigma=3)
+    # create the boolean mask 
+    bw = (blurback > .3).astype('bool')
+    bw = binary_fill_holes(bw)
+    image[bw] = 0.0    # mask out the white regions
+    image -= blurback  # subtrack the blurred background
+    # subtraction causes negative values, we need to rescale it back to 0-1 interval
+    image = img_as_int(exposure.rescale_intensity(image,in_range=(0,1)))
+    
+    return image
+
