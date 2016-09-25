6597570e6c349182c270eda7a9dabb3344b3d9bf
Lem Pie <lem@pie.com>
2016-09-25 23:43:38 +0200

    added IM7 support using ReadIM library

--- openpiv/tools.py
+++ openpiv/tools.py
@@ -32,11 +32,12 @@ import matplotlib.image as mpltimg
 from scipy import ndimage
 from skimage import filters, io
 
+import ReadIM
 
 def display_vector_field( filename, on_img=False, image_name='None', window_size=32, scaling_factor=1, **kw):
-    """ Displays quiver plot of the data stored in the file 
-    
-    
+    """ Displays quiver plot of the data stored in the file
+
+
     Parameters
     ----------
     filename :  string
@@ -53,27 +54,27 @@ def display_vector_field( filename, on_img=False, image_name='None', window_size
 
     scaling_factor : float, optional
         when on_img is True, provide the scaling factor to scale the background image to the vector field
-    
+
     Key arguments   : (additional parameters, optional)
         *scale*: [None | float]
         *width*: [None | float]
-    
-    
+
+
     See also:
     ---------
     matplotlib.pyplot.quiver
-    
-        
+
+
     Examples
     --------
     --- only vector field
-    >>> openpiv.tools.display_vector_field('./exp1_0000.txt',scale=100, width=0.0025) 
+    >>> openpiv.tools.display_vector_field('./exp1_0000.txt',scale=100, width=0.0025)
 
     --- vector field on top of image
     >>> openpiv.tools.display_vector_field('./exp1_0000.txt', on_img=True, image_name='exp1_001_a.bmp', window_size=32, scaling_factor=70, scale=100, width=0.0025)
-    
+
     """
-    
+
     a = np.loadtxt(filename)
     fig=pl.figure()
     pl.hold(True)
@@ -96,52 +97,75 @@ def display_vector_field( filename, on_img=False, image_name='None', window_size
 def imread( filename, flatten=0 ):
     """Read an image file into a numpy array
     using skimage.io.imread
-    
+
     Parameters
     ----------
     filename :  string
         the absolute path of the image file
     flatten :   bool
         True if the image is RGB color or False (default) if greyscale
-        
+
     Returns
     -------
     frame : np.ndarray
         a numpy array with grey levels
-        
-        
+
+
     Examples
     --------
-    
+
     >>> image = openpiv.tools.imread( 'image.bmp' )
-    >>> print image.shape 
+    >>> print image.shape
         (1280, 1024)
-    
-    
+
+
     """
-    
-    return io.imread( filename, as_grey = True)
+    if filename.endswith(".im7"):
+        return __readim7(filename)
+    else:
+        return io.imread(filename, as_grey = True)
+
+def __readim7(filename):
+    """
+    read im7 files from LaVision's Davis software
+    returns:
+       -two frames of images
+       -PIV parameters: scaling, dt etc...
+
+    https://bitbucket.org/fleming79/readim/overview
+    http://jekel.me/2015/Open-and-View-IM7-Files-with-Python/
+    """
+    buff, i_atts   =  ReadIM.extra.get_Buffer_andAttributeList(filename)
+    i_array, i_buff = ReadIM.extra.buffer_as_array(buff)
+
+    #clean the memory
+    del(buff)
+    ReadIM.DestroyBuffer(i_buff)
+    ReadIM.DestroyAttributeListSafe(i_atts)
+
+    return i_array[0],i_array[1]
+
 
 def imsave( filename, arr ):
     """Write an image file from a numpy array
     using scipy.misc.imread
-    
+
     Parameters
     ----------
     filename :  string
         the absolute path of the image file that will be created
     arr : 2d np.ndarray
         a 2d numpy array with grey levels
-        
+
     Example
     --------
-    
+
     >>> image = openpiv.tools.imread( 'image.bmp' )
     >>> image2 = openpiv.tools.negative(image)
     >>> imsave( 'negative-image.tif', image2)
-    
+
     """
-    
+
     if np.amax(arr) < 256 and np.amin(arr) >= 0:
         scipy.misc.imsave( filename, arr )
     else:
@@ -214,7 +238,7 @@ def find_reflexions(list_img, filename):
     imsave(filename, reflexion)
     print "done with reflexions"
     return reflexion
-            
+
 
 
 
@@ -261,59 +285,59 @@ def find_boundaries(threshold, list_img1, list_img2, filename, picname):
 
 def save( x, y, u, v, mask, filename, fmt='%8.4f', delimiter='\t' ):
     """Save flow field to an ascii file.
-    
+
     Parameters
     ----------
     x : 2d np.ndarray
-        a two dimensional array containing the x coordinates of the 
+        a two dimensional array containing the x coordinates of the
         interrogation window centers, in pixels.
-        
+
     y : 2d np.ndarray
-        a two dimensional array containing the y coordinates of the 
+        a two dimensional array containing the y coordinates of the
         interrogation window centers, in pixels.
-        
+
     u : 2d np.ndarray
         a two dimensional array containing the u velocity components,
         in pixels/seconds.
-        
+
     v : 2d np.ndarray
         a two dimensional array containing the v velocity components,
         in pixels/seconds.
-        
+
     mask : 2d np.ndarray
         a two dimensional boolen array where elements corresponding to
         invalid vectors are True.
-        
+
     filename : string
         the path of the file where to save the flow field
-        
+
     fmt : string
         a format string. See documentation of numpy.savetxt
         for more details.
-    
+
     delimiter : string
         character separating columns
-        
+
     Examples
     --------
-    
+
     >>> openpiv.tools.save( x, y, u, v, 'field_001.txt', fmt='%6.3f', delimiter='\t')
-    
+
     """
     # build output array
     out = np.vstack( [m.ravel() for m in [x, y, u, v, mask] ] )
-            
+
     # save data to file.
     np.savetxt( filename, out.T, fmt=fmt, delimiter=delimiter )
 
 def display( message ):
     """Display a message to standard output.
-    
+
     Parameters
     ----------
     message : string
         a message to be printed
-    
+
     """
     sys.stdout.write(message)
     sys.stdout.write('\n')
@@ -326,69 +350,69 @@ class Multiprocesser():
         This class is responsible of loading image datasets
         and processing them. It has parallelization facilities
         to speed up the computation on multicore machines.
-        
-        It currently support only image pair obtained from 
-        conventional double pulse piv acquisition. Support 
-        for continuos time resolved piv acquistion is in the 
+
+        It currently support only image pair obtained from
+        conventional double pulse piv acquisition. Support
+        for continuos time resolved piv acquistion is in the
         future.
-        
-        
+
+
         Parameters
         ----------
         data_dir : str
-            the path where image files are located 
-            
+            the path where image files are located
+
         pattern_a : str
-            a shell glob patter to match the first 
+            a shell glob patter to match the first
             frames.
-            
+
         pattern_b : str
             a shell glob patter to match the second
-            frames. if None, then the list is sequential, 001.tif, 002.tif 
+            frames. if None, then the list is sequential, 001.tif, 002.tif
 
         Examples
         --------
         >>> multi = openpiv.tools.Multiprocesser( '/home/user/images', 'image_*_a.bmp', 'image_*_b.bmp')
-    
+
         """
         # load lists of images
-         
+
         self.files_a = sorted( glob.glob( os.path.join( os.path.abspath(data_dir), pattern_a ) ) )
-        
+
         if pattern_b is None:
             self.files_b = self.files_a[1:]
             self.files_a = self.files_a[:-1]
-        else:    
+        else:
             self.files_b = sorted( glob.glob( os.path.join( os.path.abspath(data_dir), pattern_b ) ) )
-        
+
         # number of images
         self.n_files = len(self.files_a)
-        
+
         # check if everything was fine
         if not len(self.files_a) == len(self.files_b):
             raise ValueError('Something failed loading the image file. There should be an equal number of "a" and "b" files.')
-            
+
         if not len(self.files_a):
             raise ValueError('Something failed loading the image file. No images were found. Please check directory and image template name.')
 
     def run( self, func, n_cpus=1 ):
         """Start to process images.
-        
+
         Parameters
         ----------
-        
-        func : python function which will be executed for each 
+
+        func : python function which will be executed for each
             image pair. See tutorial for more details.
-        
+
         n_cpus : int
             the number of processes to launch in parallel.
             For debugging purposes use n_cpus=1
-        
+
         """
 
         # create a list of tasks to be executed.
         image_pairs = [ (file_a, file_b, i) for file_a, file_b, i in zip( self.files_a, self.files_b, xrange(self.n_files) ) ]
-        
+
         # for debugging purposes always use n_cpus = 1,
         # since it is difficult to debug multiprocessing stuff.
         if n_cpus > 1:
@@ -397,11 +421,11 @@ class Multiprocesser():
         else:
             for image_pair in image_pairs:
                 func( image_pair )
-                
+
 
 def negative( image):
     """ Return the negative of an image
-    
+
     Parameter
     ----------
     image : 2d np.ndarray of grey levels
@@ -416,35 +440,35 @@ def negative( image):
 
 def display_windows_sampling( x, y, window_size, skip=0,  method='standard'):
     """ Displays a map of the interrogation points and windows
-    
-    
+
+
     Parameters
     ----------
     x : 2d np.ndarray
-        a two dimensional array containing the x coordinates of the 
+        a two dimensional array containing the x coordinates of the
         interrogation window centers, in pixels.
-        
+
     y : 2d np.ndarray
-        a two dimensional array containing the y coordinates of the 
+        a two dimensional array containing the y coordinates of the
         interrogation window centers, in pixels.
 
     window_size : the interrogation window size, in pixels
-    
-    skip : the number of windows to skip on a row during display. 
+
+    skip : the number of windows to skip on a row during display.
            Recommended value is 0 or 1 for standard method, can be more for random method
            -1 to not show any window
 
     method : can be only <standard> (uniform sampling and constant window size)
                          <random> (pick randomly some windows)
-    
+
     Examples
     --------
-    
+
     >>> openpiv.tools.display_windows_sampling(x, y, window_size=32, skip=0, method='standard')
 
-    
+
     """
-    
+
     fig=pl.figure()
     pl.hold(True)
     if skip < 0 or skip +1 > len(x[0])*len(y):
@@ -483,4 +507,3 @@ def display_windows_sampling( x, y, window_size, skip=0,  method='standard'):
             raise ValueError('method not valid: choose between standard and random')
     pl.draw()
     pl.show()
-   
