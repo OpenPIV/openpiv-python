e9d1889281471159850ee937723daa6e14e4742b
Alex Liberzon <alex.liberzon@gmail.com>
2016-11-09 20:27:12 +0200

    updated tutorial-part1,2,3 to call openpiv modules

--- openpiv/examples/tutorial-part1/tutorial-part2small.py
+++ openpiv/examples/tutorial-part1/tutorial-part2small.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python
 import sys
-sys.path.append('/Users/alex/Documents/OpenPIV/alexlib/openpiv-python')
 
-import openpiv.tools
-import openpiv.process
-import openpiv.scaling
+if 'OpenPIV' not in sys.path:
+    sys.path.append('/Users/alex/Documents/OpenPIV/alexlib/openpiv-python')
+
+from openpiv import tools, validation, process, filters, scaling, pyprocess
 import numpy as np
+import matplotlib.pyplot as plt
 
-from pylab import * 
-
-frame_a  = openpiv.tools.imread('exp1_001_a.bmp')
-frame_b  = openpiv.tools.imread('exp1_001_b.bmp' )
+frame_a  = tools.imread('exp1_001_a.bmp')
+frame_b  = tools.imread('exp1_001_b.bmp' )
 
 
 frame_a = frame_a[:128,:128]
@@ -20,33 +20,37 @@ imshow(np.c_[frame_a,frame_b],cmap=cm.gray)
 show()
 
 
-u, v, sig2noise = openpiv.process.extended_search_area_piv( 
-                                                           frame_a.astype(np.int32), 
-frame_b.astype(np.int32), 
-window_size=24, overlap=12, 
-dt=0.02, search_area_size=32, 
-sig2noise_method='peak2peak' )
+u, v, sig2noise = process.extended_search_area_piv(
+    frame_a.astype(np.int32), 
+    frame_b.astype(np.int32), 
+    window_size=24, overlap=12, 
+    dt=0.02, search_area_size=32, 
+    sig2noise_method='peak2peak' )
 
-x, y = openpiv.process.get_coordinates( image_size=frame_a.shape, window_size=24, overlap=12 )
-
-
-
-quiver(x,y,u,v)
-show()
+x, y = process.get_coordinates( image_size=frame_a.shape, window_size=24, overlap=12 )
 
 
-u, v= openpiv.pyprocess.piv( frame_a, frame_b, corr_method='fft', 
+plt.figure()
+plt.quiver(x,y,u,v)
+plt.show()
+
+
+u, v, s2n = pyprocess.piv( frame_a, frame_b, corr_method='fft', 
                             window_size=32, overlap=12, dt=0.02, sig2noise_method='peak2peak' )
-x, y = openpiv.pyprocess.get_coordinates( image_size=frame_a.shape, window_size=24, overlap=12 )
+x, y = pyprocess.get_coordinates( image_size=frame_a.shape, window_size=32, overlap=12 )
 
-quiver(x,y,u,v)
-show()
+plt.figure()
+plt.quiver(x,y,u,v)
+plt.show()
 
 
-u, v= openpiv.pyprocess.piv( frame_a, frame_b, corr_method='direct', 
-                            window_size=32, overlap=12, dt=0.02, sig2noise_method='peak2peak' )
-x, y = openpiv.pyprocess.get_coordinates( image_size=frame_a.shape, 
+u, v, s2n = pyprocess.piv( frame_a, frame_b, corr_method='direct', 
+                            window_size=24, overlap=12, dt=0.02, 
+                            sig2noise_method='peak2peak',
+                            search_size = 32)
+x, y = pyprocess.get_coordinates( image_size=frame_a.shape, 
                                          window_size=24, overlap=12 )
 
-quiver(x,y,u,v)
-show()
+plt.figure()
+plt.quiver(x,y,u,v)
+plt.show()
