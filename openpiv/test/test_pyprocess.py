74217567a9a875546c7841da3eff5ff17331694f
Alex Liberzon <alex.liberzon@gmail.com>
2016-11-17 20:52:32 +0200

    updated tests with the more realistic bounds for error (0.5 pix)

--- openpiv/test/test_pyprocess.py
+++ openpiv/test/test_pyprocess.py
@@ -20,7 +20,7 @@ def test_piv():
     frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
     u,v = piv(frame_a,frame_b,window_size=32)
-    print u,v
+    # print u,v
     assert(np.max(np.abs(u-3)) < 0.2)
     assert(np.max(np.abs(v+2)) < 0.2)
     
@@ -31,7 +31,7 @@ def test_piv_smaller_window():
     frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,-3,axis=1),-2,axis=0)
     u,v = piv(frame_a,frame_b,window_size=16,search_size=32)
-    print u,v
+    # print u,v
     assert(np.max(np.abs(u+3)) < 0.2)
     assert(np.max(np.abs(v-2)) < 0.2)
     
@@ -42,8 +42,8 @@ def test_extended_search_area():
     frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
     u,v = piv(frame_a,frame_b,window_size=16,search_size=32,overlap=0)
-    print u,v
-    assert(np.max(np.abs(u-3)+np.abs(v+2)) <= 0.2)
+    # print u,v
+    assert(np.max(np.abs(u-3)+np.abs(v+2)) <= 0.5)
     
 def test_extended_search_area_overlap():
     """ test of the extended area PIV """
@@ -52,7 +52,7 @@ def test_extended_search_area_overlap():
     frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
     u,v = piv(frame_a,frame_b,window_size=16,search_size=32,overlap=8)
-    print u,v
+    # print u,v
     assert(np.max(np.abs(u-3)+np.abs(v+2)) <= 0.3)
     
 def test_extended_search_area_sig2noise():
@@ -74,5 +74,5 @@ def test_process_extended_search_area():
     u,v = process.extended_search_area_piv(frame_a.astype(np.int32),
                                            frame_b.astype(np.int32),
 window_size=16,search_area_size=32,dt=1,overlap=0)
-    print u,v
+    # print u,v
     assert(np.max(np.abs(u[:-1,:-1]-3)+np.abs(v[:-1,:-1]+2)) <= 0.2)
