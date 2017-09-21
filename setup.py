b8661840b2789328cfe1ecd10a2f6f8c84da3f4c
Alex Liberzon <alexlib@users.noreply.github.com>
2017-09-21 13:01:38 +0300

    0.20.7 -> 0.20.8
    updated the progressbar2, need to push to pipy and conda-forge

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ packages = ['openpiv', 'openpiv.ui']
 
 
 setup(  name = "OpenPIV",
-        version = "0.20.7",
+        version = "0.20.8",
         author = "OpenPIV contributors",
         author_email = "openpiv-users@googlegroups.com",
         description = "An open source software for PIV data analysis",
