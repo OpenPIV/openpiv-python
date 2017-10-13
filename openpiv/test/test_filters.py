0e9e5f027676daa9730562439bb2bac068817983
Alex Liberzon <alex.liberzon@gmail.com>
2017-10-13 20:25:34 +0300

    replace_outliers do not change the arrays u,v but return uf,vf as new copies, test_filters are complete

--- openpiv/test/test_filters.py
+++ openpiv/test/test_filters.py
@@ -1,20 +1,62 @@
-import unittest
+import openpiv.filters as filters
+import numpy as np
+from skimage.util import random_noise
+from skimage import img_as_ubyte
 
-class TestStringMethods(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+def test_import():
+    """ test of the simplest PIV run """
+    try:
+        import openpiv.filters as filters
+    except:
+        raise ImportError
+    
+def test_gaussian_kernel():
+    """ test of a _gaussian_kernel """
+    
+    assert(np.allclose(filters._gaussian_kernel(1), 
+    np.array([[ 0.04491922,  0.12210311,  0.04491922],
+       [ 0.12210311,  0.33191066,  0.12210311],
+       [ 0.04491922,  0.12210311,  0.04491922]])))
+       
+    assert(np.isnan(filters._gaussian_kernel(0))) # issues a Warning
+    
+    
+def test_gaussian():
+    """ test of a Gaussian filter """
+    u = np.ones((3,3))
+    v = np.eye(3)
+    uf,vf = filters.gaussian(u,v,1)
+    assert(np.allclose(uf,np.array([[ 0.62103611,  0.78805844,  0.62103611],
+        [ 0.78805844,  1.        ,  0.78805844],
+        [ 0.62103611,  0.78805844,  0.62103611]])))
+    assert(np.allclose(vf,np.array([[ 0.37682989,  0.24420622,  0.04491922],
+        [ 0.24420622,  0.42174911,  0.24420622],
+        [ 0.04491922,  0.24420622,  0.37682989]])))
+        
+def test_replace_nans():
+    """ test of NaNs inpainting """
+    u = np.nan*np.ones((5,5))
+    u[2,2] = 1
+    filters.replace_nans(u,2,1e-3)
+    assert(~np.all(np.isnan(u)))
+    v = np.ones((9,9))
+    v[1:-1,1:-1] = np.nan
+    u = v.copy()
+    filters.replace_nans(u,1,1e-3,method='disk')
+    assert(np.sum(np.isnan(u)) == 9) # central core is nan
+    u = v.copy()
+    filters.replace_nans(u,2,1e-3,method='disk')
+    assert(np.allclose(np.ones((9,9)),u))
+    
+    
+def test_replace_outliers():
+    """ test of replacing outliers """
+    v = np.ones((9,9))
+    v[1:-1,1:-1] = np.nan
+    u = v.copy()
+    uf,vf = filters.replace_outliers(u,v)
+    assert(np.sum(np.isnan(u))==7**2)
+    assert(np.allclose(np.ones((9,9)),uf))
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
-
-if __name__ == '__main__':
-    unittest.main()
+    
