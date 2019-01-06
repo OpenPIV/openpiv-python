f6be4b80cca9c925f9b290bd554cf734847c6a6b
Alex Liberzon <alexlib@users.noreply.github.com>
2019-01-06 22:50:48 +0200

    Update masking_tutorial.py

--- openpiv/examples/masking_tutorial/masking_tutorial.py
+++ openpiv/examples/masking_tutorial/masking_tutorial.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
-
+import numpy as np
 import openpiv.tools
 import openpiv.process
 import openpiv.scaling
 import openpiv.preprocess
+import openpiv.validation
+import openpiv.filters
 from skimage import io
+from skimage.io import imshow
 
-# %pylab inline
-
-# <codecell>
 
 file_a = 'Camera1-0101.tif'
 file_b = 'Camera1-0102.tif'
@@ -20,14 +16,10 @@ im_a  = io.imread( file_a )
 im_b  = io.imread( file_b )
 imshow(np.c_[im_a,im_b],cmap='gray')
 
-# <codecell>
-
 # let's crop the region of interest
 frame_a =  im_a[380:1980,0:1390]
 frame_b =  im_b[380:1980,0:1390]
 imshow(np.c_[frame_a,frame_b],cmap='gray')
-
-# <codecell>
 
 # Process the original cropped image and see the OpenPIV result:
 
@@ -54,21 +46,21 @@ x, y, u, v = openpiv.scaling.uniform(x, y, u, v, scaling_factor = 96.52 )
 openpiv.tools.save(x, y, u, v, mask, 'test.txt', fmt='%9.6f', delimiter='\t')
 openpiv.tools.display_vector_field('test.txt', scale=50, width=0.002)
 
-# <codecell>
+
 
 # masking using not optimal choice of the methods or parameters:
 masked_a = openpiv.preprocess.dynamic_masking(frame_a,method='edges',filter_size=7,threshold=0.005)
 masked_b = openpiv.preprocess.dynamic_masking(frame_b,method='intensity',filter_size=3,threshold=0.0)
 imshow(np.c_[masked_a,masked_b],cmap='gray')
 
-# <codecell>
+
 
 # masking using optimal (manually tuned) set of parameters and the right method:
 masked_a = openpiv.preprocess.dynamic_masking(frame_a,method='edges',filter_size=7,threshold=0.01)
 masked_b = openpiv.preprocess.dynamic_masking(frame_b,method='edges',filter_size=7,threshold=0.01)
 imshow(np.c_[masked_a,masked_b],cmap='gray')
 
-# <codecell>
+
 
 # Process the masked cropped image and see the OpenPIV result:
 
