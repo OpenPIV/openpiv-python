31c7d1d0afa1067b4b43515aa9eb927b34650309
TKaeufer <55536110+TKaeufer@users.noreply.github.com>
2019-10-15 22:46:43 +0200

    test fix

--- openpiv/test/test_windef.py
+++ openpiv/test/test_windef.py
@@ -6,8 +6,9 @@ Created on Fri Oct  4 14:33:21 2019
 """
 
 
-from windef import first_pass as first
-from windef import multipass_img_deform as multi
+
+
+
 
 import numpy as np
 
@@ -17,6 +18,10 @@ from skimage import img_as_ubyte
     
 import warnings
 
+
+import windef
+#from windef import first_pass as first
+#from windef import multipass_img_deform as multi
 #this test are created only to test the displacemetn evaluation of the function.
 #the validation methods are not tested here ant therefore are disabled.
 
@@ -29,7 +34,7 @@ def test_first_pass_circ():
         warnings.simplefilter("ignore")
         frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
-    x, y, u, v, sig2noise_ratio= first(frame_a, frame_b, window_size=64, overlap=32,iterations=1,correlation_method='circular',
+    x, y, u, v, sig2noise_ratio= windef.first_pass(frame_a, frame_b, window_size=64, overlap=32,iterations=1,correlation_method='circular',
                                        subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
     # print u,v
     #print('test')
@@ -49,12 +54,12 @@ def test_multi_pass_circ():
         warnings.simplefilter("ignore")
         frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
-    x, y, u, v, sig2noise_ratio= first(frame_a, frame_b, window_size[0], overlap[0],iterations,correlation_method='circular', subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
+    x, y, u, v, sig2noise_ratio= windef.first_pass(frame_a, frame_b, window_size[0], overlap[0],iterations,correlation_method='circular', subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
     u_old=u
     v_old=v
     i=1
     for i in range(2,iterations+1):
-        x, y, u, v, sig2noise_ratio, mask=multi(frame_a, frame_b, window_size[i-1], overlap[i-1],iterations,i, x, y, u, v, correlation_method='circular',
+        x, y, u, v, sig2noise_ratio, mask=windef.multipass_img_deform(frame_a, frame_b, window_size[i-1], overlap[i-1],iterations,i, x, y, u, v, correlation_method='circular',
                              subpixel_method='gaussian', do_sig2noise=False, sig2noise_method='peak2peak', sig2noise_mask=2,
                              MinMaxU=(-100, 50), MinMaxV=(-50, 50), std_threshold=1000000, median_threshold=200000,median_size=1, filter_method='localmean',
                              max_filter_iteration=10, filter_kernel_size=2, interpolation_order=3)
@@ -74,7 +79,7 @@ def test_first_pass_lin():
         warnings.simplefilter("ignore")
         frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
-    x, y, u, v, sig2noise_ratio= first(frame_a, frame_b, window_size=64, overlap=32,iterations=1,correlation_method='linear',
+    x, y, u, v, sig2noise_ratio= windef.first_pass(frame_a, frame_b, window_size=64, overlap=32,iterations=1,correlation_method='linear',
                                        subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
     # print u,v
     #print('test')
@@ -94,12 +99,12 @@ def test_multi_pass_lin():
         warnings.simplefilter("ignore")
         frame_a = img_as_ubyte(frame_a)
     frame_b = np.roll(np.roll(frame_a,3,axis=1),2,axis=0)
-    x, y, u, v, sig2noise_ratio= first(frame_a, frame_b, window_size[0], overlap[0],iterations,correlation_method='linear', subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
+    x, y, u, v, sig2noise_ratio= windef.first_pass(frame_a, frame_b, window_size[0], overlap[0],iterations,correlation_method='linear', subpixel_method='gaussian',do_sig2noise=True, sig2noise_method='peak2peak', sig2noise_mask=2)
     u_old=u
     v_old=v
     i=1
     for i in range(2,iterations+1):
-        x, y, u, v, sig2noise_ratio, mask=multi(frame_a, frame_b, window_size[i-1], overlap[i-1],iterations,i, x, y, u, v, correlation_method='linear',
+        x, y, u, v, sig2noise_ratio, mask=windef.multipass_img_deform(frame_a, frame_b, window_size[i-1], overlap[i-1],iterations,i, x, y, u, v, correlation_method='linear',
                              subpixel_method='gaussian', do_sig2noise=False, sig2noise_method='peak2peak', sig2noise_mask=2,
                              MinMaxU=(-100, 50), MinMaxV=(-50, 50), std_threshold=1000000, median_threshold=200000,median_size=1, filter_method='localmean',
                              max_filter_iteration=10, filter_kernel_size=2, interpolation_order=3)
