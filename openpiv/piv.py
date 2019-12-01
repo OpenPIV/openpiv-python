35e006ee58d727ef854fb73065d92d24dcb321a2
Alex Liberzon <747110+alexlib@users.noreply.github.com>
2019-12-01 21:03:42 +0200

    added cropped images

--- openpiv/piv.py
+++ openpiv/piv.py
@@ -1,4 +1,4 @@
-def piv(im1=r'C:\Users\alex\Downloads\B005_1.tif',im2=r'C:\Users\alex\Downloads\B005_2.tif'):
+def piv():
     """
     Simplest PIV run on the pair of images using default settings
 
@@ -7,22 +7,32 @@ def piv(im1=r'C:\Users\alex\Downloads\B005_1.tif',im2=r'C:\Users\alex\Downloads\
 
     """
 
+
+
     import imageio
     import numpy as np
     import matplotlib.pyplot as plt
 
     from openpiv import process
-    
+    import pkg_resources as pkg
+    import os
+
+
+    # if im1 is None and im2 is None:
+    im1 = pkg.resource_filename('openpiv',os.path.join('examples','test5','frame_a.tif'))
+    im2 = pkg.resource_filename('openpiv',os.path.join('examples','test5','frame_b.tif'))
+
+
     frame_a = imageio.imread(im1)
     frame_b = imageio.imread(im2)
-
+    
     u, v = process.extended_search_area_piv(frame_a.astype(np.int32),frame_b.astype(np.int32),window_size=32,overlap=16)
     x, y = process.get_coordinates( image_size=frame_a.shape, 
                                window_size=32, overlap=16)
 
     plt.figure(figsize=(12,12))
     plt.imshow(frame_a,cmap=plt.cm.gray,alpha=0.8,origin='lower')
-    plt.quiver(x,y,u,-v,scale=50,color='r')
+    plt.quiver(x,y,u,v,scale=50,color='r')
+    plt.gca().invert_yaxis()
     plt.show()
-    # plt.gca().invert_yaxis()
     
