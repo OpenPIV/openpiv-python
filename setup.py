43d23d4211cd9d435c36973798b2ff1737fadeb3
Alex Liberzon <alexlib@users.noreply.github.com>
2019-12-07 12:49:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,12 +18,13 @@ extensions = cythonize(extensions,include_path = [numpy.get_include()])
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
     name = "OpenPIV",
-    version ='0.21.8',
+    version ='0.21.8b',
     cmdclass = {'build_ext': build_ext},
     ext_modules = extensions,
     packages=find_packages(),
