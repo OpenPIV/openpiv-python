2f1efd9e33586356def4c6c200cdb9af22aa8785
Alex Liberzon <alexlib@users.noreply.github.com>
2020-09-22 01:39:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name = "OpenPIV",
-    version ='0.22.2',
+    version ='0.22.3',
     cmdclass = {'build_ext': build_ext},
     ext_modules = extensions,
     packages=find_packages(),
