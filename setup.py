35782a9fde745d05ff51bcd5af8bc342a290cbd4
Alex Liberzon <alexlib@users.noreply.github.com>
2022-06-12 22:54:30 +0300

    updated to 0.24.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="OpenPIV",
-    version='0.23.9',
+    version='0.24.0',
     packages=find_packages(),
     include_package_data=True,
     long_description=long_description,
