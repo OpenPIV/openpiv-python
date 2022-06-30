4a035deec4e8678402a7b497f2f9234e8084e21e
Alex Liberzon <alexlib@users.noreply.github.com>
2022-06-30 09:39:52 +0300

    update to 0.24.1
    Changes:
    - natural sort for the list of files in windef 
    - static masking for the windef

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="OpenPIV",
-    version='0.24.0',
+    version='0.24.1',
     packages=find_packages(),
     include_package_data=True,
     long_description=long_description,
