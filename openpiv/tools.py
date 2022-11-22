24cafb19e5d1caea45ff220944f7f2900ac5e459
Alex Liberzon <alexlib@users.noreply.github.com>
2022-11-22 22:38:27 +0200

    return to scientific notation tools.py

--- openpiv/tools.py
+++ openpiv/tools.py
@@ -390,7 +390,7 @@ def save(
     v: np.ndarray, 
     flags: Optional[np.ndarray] = None,
     mask: Optional[np.ndarray] = None,
-    fmt: str="%.4ef", 
+    fmt: str="%.4e", 
     delimiter: str="\t",
     )-> None:
     """Save flow field to an ascii file.
