2429d0ab39e98fce654fae1ab1eef0a93d50e6f9
erich-zimmer <erich_zimmer@hotmail.com>
2024-08-21 13:38:14 -0500

    Oops, wrong file

--- openpiv/test/test_calib_target_grids.py
+++ openpiv/test/test_calib_target_grids.py
@@ -7,370 +7,77 @@ from numpy.testing import (assert_equal, assert_allclose,
                            assert_array_equal, assert_)
 
 from openpiv.calibration import poly_model
-from openpiv.calibration.calib_utils import get_reprojection_error, get_los_error
+from openpiv.calibration.calib_utils import get_simple_grid, get_asymmetric_grid
 
-file = os.path.join(os.path.dirname(__file__),"test_calibration_points.npz")
 
-def test_parameters_input():
-    with pytest.raises(TypeError):
-         # missing camera name
-        poly_model.camera()
-        
-        # missing resolution
-        poly_model.camera(
-            "name"
-        )
-                                 
-    with pytest.raises(ValueError):
-        # name is not a string
-        poly_model.camera(
-            0,
-            resolution=[0, 0]
-        )
-        
-        # not two element tuple
-        poly_model.camera(
-            "name",
-            resolution=[0]
-        )
-        
-        # not 2D
-        poly_model.camera(
-            "name",
-            resolution=[0, 0],
-            poly_wi = np.zeros(19)
-        )
-        
-        # not 2D
-        poly_model.camera(
-            "name",
-            resolution=[0, 0],
-            poly_iw = np.zeros(19)
-        )
-        
-        # not correct shape
-        poly_model.camera(
-            "name",
-            resolution=[0, 0],
-            poly_wi = np.zeros((10, 10))
-        )
-        
-        # not correct shape
-        poly_model.camera(
-            "name",
-            resolution=[0, 0],
-            poly_iw = np.zeros((10, 10))
-        )
-        
-
-def test_parameters_initialization():
-    cam = poly_model.camera(
-        "name",
-        resolution=[0, 0]
+def test_simple_grid_01():
+    grid_shape = (9, 9)
+    origin = np.array(grid_shape) // 2
+    z_plane = 0
+    spacing = 10
+    
+    grid = get_simple_grid(
+        grid_shape[0], grid_shape[1],
+        z_plane,
+        origin[0], origin[1], 
+        spacing=spacing
     )
     
-    assert_(hasattr(cam, "name"))
-    assert_(hasattr(cam, "resolution"))
-    assert_(hasattr(cam, "poly_wi"))
-    assert_(hasattr(cam, "poly_iw"))
-    assert_(hasattr(cam, "dlt"))
-    assert_(hasattr(cam, "dtype"))
+    # check horizontal and vertical spacing
+    assert_((grid[0, 1] - grid[0, 0]) == spacing)
+    assert_((grid[1, 1] - grid[1, 9]) == spacing)
     
-    assert_(len(cam.resolution) == 2)
-            
+    # make sure z-plane is correct
+    assert_(np.all(grid[2, :] == z_plane))
+    
+    # check grid shape
     assert_equal(
-        cam.poly_wi.shape,
-        [19, 2]
+        grid.shape, 
+        (3, np.prod(grid_shape))
     )
+    
+    # make sure origin is 0
+    grid = grid.reshape((3, grid_shape[0], grid_shape[1]))
+    
+    origin = grid[:2, origin[0], origin[1]]
+    
+    assert_(origin[0] == 0)
+    assert_(origin[1] == 0)
+    
+    
+def test_asymmetric_grid_01():
+    grid_shape = (9, 9)
+    origin = np.array(grid_shape) // 2
+    
+    z_plane = 0
+    spacing = 10
+    
+    grid = get_asymmetric_grid(
+        grid_shape[0], grid_shape[1],
+        z_plane,
+        origin[0], origin[1], 
+        spacing=spacing
+    )
+    
+    # check horizontal and vertical spacing
+    assert_((grid[0, 1] - grid[0, 0]) == (spacing * 2))
+    assert_((grid[1, 1] - grid[1, 9]) == (spacing * 2))
+    
+    # make sure z-plane is correct
+    assert_(np.all(grid[2, :] == z_plane))
+    
+    # check grid shape
+    grid_length = int(np.prod(grid_shape) / 2 + 0.5)
     
     assert_equal(
-        cam.poly_iw.shape,
-        [19, 3]
+        grid.shape, 
+        (3, grid_length)
     )
     
-    assert_(cam.dtype in ["float32", "float64"])
-
-
-@pytest.mark.parametrize("case", (1, 2, 3))
-def test_minimization_01(
-    case: int
-):    
-    cal_data = np.load(file)
+    # make sure origin is 0 
+    origin_ind = int(grid_length / 2)
     
-    cal_obj_points = cal_data[f"obj_points_{case}"]
-    cal_img_points = cal_data[f"img_points_{case}"]
+    origin = grid[:2, origin_ind]
     
-    cam = poly_model.camera(
-        "poly",
-        resolution = [512, 512],
-    )
-    
-    cam.minimize_params(
-        cal_obj_points,
-        cal_img_points
-    )
-    
-    RMSE = get_reprojection_error(
-        cam,
-        cal_obj_points,
-        cal_img_points
-    )
-    
-    assert_(RMSE < 1e-2)
-
-
-@pytest.mark.parametrize("case", (1, 2))
-def test_projection_01(
-    case: int
-):
-    cal_data = np.load(file)
-    
-    cal_obj_points = cal_data[f"obj_points_{case}"]
-    cal_img_points = cal_data[f"img_points_{case}"]
-    
-    cam = poly_model.camera(
-        "poly",
-        resolution = [512, 512],
-    )
-    
-    cam.minimize_params(
-        cal_obj_points,
-        cal_img_points
-    )
-    
-    obj_points = np.random.rand(3, 32)
-    obj_points[0, :] = np.int32(obj_points[0, :] * 50)
-    obj_points[1, :] = np.int32(obj_points[1, :] * 50)
-    obj_points[2, :] = np.int32(obj_points[2, :] * 10)
-    
-    obj_points = obj_points.astype("float64", copy=False)
-    
-    img_points = cam.project_points(
-        obj_points
-    )
-    
-    recon_obj_points = cam.project_to_z(
-        img_points,
-        obj_points[2]
-    )
-    
-    assert_array_almost_equal(
-        obj_points,
-        recon_obj_points,
-        decimal=2
-    )
-
-
-@pytest.mark.parametrize("case", (1, 2, 3))
-def test_projection_02(
-    case: int
-):
-    cal_data = np.load(file)
-    
-    cal_obj_points = cal_data[f"obj_points_{case}"]
-    cal_img_points = cal_data[f"img_points_{case}"]
-    
-    cam = poly_model.camera(
-        "poly",
-        resolution = [512, 512],
-    )
-    
-    cam.minimize_params(
-        cal_obj_points,
-        cal_img_points
-    )
-    
-    x, y = cam.project_points(
-        cal_obj_points
-    )
-    
-    assert_array_almost_equal(
-        x, cal_img_points[0],
-        decimal=2
-    )
-    
-    assert_array_almost_equal(
-        y, cal_img_points[1],
-        decimal=2
-    )
-    
-# Test case 1 and 3 needs higher thresholds due to camera malignment
-@pytest.mark.parametrize("case", (1, 2, 3))
-def test_projection_03(
-    case: int
-):    
-    cal_data = np.load(file)
-    
-    cal_obj_points = cal_data[f"obj_points_{case}"]
-    cal_img_points = cal_data[f"img_points_{case}"]
-    
-    cam = poly_model.camera(
-        "poly",
-        resolution = [512, 512],
-    )
-    
-    cam.minimize_params(
-        cal_obj_points,
-        cal_img_points
-    )
-    
-    RMSE_0 = get_los_error(
-        cam,
-        z = -10
-    )
-    
-    RMSE_1 = get_los_error(
-        cam,
-        z = 0
-    )
-    
-    RMSE_2 = get_los_error(
-        cam,
-        z = 10
-    )
-    
-    assert_(RMSE_0 < 0.5)
-    assert_(RMSE_1 < 0.5)
-    assert_(RMSE_2 < 0.5)
-    
-
-@pytest.mark.parametrize("case", ((1, 2), (1, 3), (2, 3), (1, 2, 3)))
-def test_line_intersect_01(
-    case: tuple
-):
-    cal_data = np.load(file)
-    n_cams = len(case)
-    
-    cal_obj_points = []
-    cal_img_points = []
-    for cam in case:
-        cal_obj_points.append(cal_data[f"obj_points_{cam}"])
-        cal_img_points.append(cal_data[f"img_points_{cam}"])
-    
-    cams = []
-    for cam in range(n_cams):
-        cam_1 = poly_model.camera(
-            "dummy",
-            resolution = [512, 512]
-        )
-
-        cam_1.minimize_params(
-            cal_obj_points[cam],
-            cal_img_points[cam]
-        )
-        
-        cams.append(cam_1)
-
-    test_object_points = np.array(
-        [
-            [0, 0, 0],
-            [1, 2, 3],
-            [3, 2, 1],
-            [15, 15, -15],
-            [15, 15, 0],
-            [15, 15, 15]
-        ],
-        dtype=cam_1.dtype
-    ).T
-    
-    test_image_points = []
-    for cam in range(n_cams):
-        test_image_points.append(
-            cams[cam].project_points(
-                test_object_points
-            )
-        )
-
-    recon_obj_points = poly_model.multi_line_intersect(
-        cams,
-        test_image_points
-    )
-    
-    assert_array_almost_equal(
-        test_object_points,
-        recon_obj_points,
-        decimal=0
-    )
-    
-    
-def test_save_parameters_1():
-    cam = poly_model.camera(
-        "dummy",
-        resolution = [512, 512]
-    )
-        
-    cam.save_parameters(
-        "."
-    )
-
-
-def test_save_parameters_2():
-    cam = poly_model.camera(
-        "dummy",
-        resolution = [512, 512]
-    )
-
-        
-    cam.save_parameters(
-        ".", "saved_params"
-    )
-
-    
-def test_load_parameters_1():
-    cam = poly_model.camera(
-        "dummy",
-        resolution = [512, 512]
-    )
-    
-    with pytest.raises(FileNotFoundError):
-        cam.load_parameters(
-            ".",
-            "does not exist (hopefully)"
-        )
-    
-
-def test_load_parameters_2():
-    cam_orig = poly_model.camera(
-        "dummy",
-        resolution = [512, 512]
-    )
-    
-    cam_new = poly_model.camera(
-        "dummy",
-        resolution = [512, 512]
-    )
-        
-    cam_orig.save_parameters(
-        ".",
-        "dummy"
-    )
-    
-    cam_new.load_parameters(
-        ".",
-        "dummy"
-    )
-    
-    assert_array_equal(
-        cam_orig.name, 
-        cam_new.name
-    )
-    
-    assert_array_equal(
-        cam_orig.resolution, 
-        cam_new.resolution
-    )
-    
-    assert_array_equal(
-        cam_orig.poly_wi, 
-        cam_new.poly_wi
-    )
-    
-    assert_array_equal(
-        cam_orig.poly_iw, 
-        cam_new.poly_iw
-    )
-    
-    assert_array_equal(
-        cam_orig.dtype, 
-        cam_new.dtype
-    )
+    assert_(origin[0] == 0)
+    assert_(origin[1] == 0)
